d1bd646bdf4ac3db11a6bb3f8108f76fb3bb9904
lucasfreitas-dev <lucas.freitas.dev@gmail.com>
2022-10-30 16:12:29 -0300

    refactor: Improve handling exceptions

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from urllib import response
 import requests as rq
 import pandas as pd
 import os
@@ -6,13 +7,19 @@ import time
 uf_list = [ 'ac' , 'al' , 'ap' , 'am' , 'ba' , 'ce' , 'df' , 'es' , 'go' , 'ma' , 'mt' , 'ms' , 'mg' , 'pr' , 'pb' , 'pa' , 'pe' , 'pi' , 'rj' , 'rn' , 'rs' , 'ro' , 'rr' , 'se' , 'sc' , 'sp' , 'to', 'zz' ]
 
 def fetch_data_json(UF):
-    return rq.get(f"https://resultados.tse.jus.br/oficial/ele2022/545/dados-simplificados/{UF}/{UF}-c0001-e000545-r.json").json()
+    
+    try:
+        response = rq.get(f"https://resultados.tse.jus.br/oficial/ele2022/545/dados-simplificados/{UF}/{UF}-c0001-e000545-r.json").json()
+    except:
+        print(f"## Fail to get data from UF: {UF}")
+        return
+    return response
 
 def check_and_update_data(UF):
     old_csv = pd.read_csv(f"{UF}.csv")
     new_response = pd.DataFrame(fetch_data_json(UF))
 
-    if(old_csv["hg"][0] != new_response["hg"][0]):
+    if("hg" in new_response and old_csv["hg"][0] != new_response["hg"][0]):
         pd.concat([old_csv, new_response])
 
 def create_csv(UF):
